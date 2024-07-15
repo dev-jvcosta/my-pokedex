bd553256d8796975ed190fed78d549caff532b37
dev.jvcosta <dev.jvcosta@gmail.com>
2024-07-15 15:12:54 +0000

    refactor: - Manipulando o resultado da requisição através de uma Promise;
    - Aprendendo o conceito de arrow function;

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,31 @@ const limit = 10
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function(response) {
-    console.log('Será executado aqui posteriomente');
-    console.log(response);
-})
+
+//ESTRUTURA DE INTERFACE DE UM FETCH
+
+// validando o sucesso da requisição
+fetch(url)
+    .then((response) => {
+
+        // retorna um objeto response Body convertido em json 
+        return response.json();
+    })
+    .then((jsonBody) => {
+
+        // recebe o jsonBody do promise anterior e exibe no console
+        console.log(jsonBody);
+    })
+    .catch((error) => {
+
+        // o .catch prmite verificar se a requisição falhou ao devolver a resposta.
+        console.log('Erro na requisição');
+        console.error(error);})
+
+    .finally(()=>{
+        
+        // finaliza a requisição independente do sucesso ou fracasso anterior;
+        console.log('Requisição concluída');
+    });
 
 console.log(`Será executado aqui primeiro `);
