b8192f89fae8d9f8534d7c8d376bb6d32a2d519f
dev.jvcosta <dev.jvcosta@gmail.com>
2024-07-17 14:39:11 +0000

    refactor: - Separando o consumo da API da manipulação de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,7 @@
 /* REALIZAR O END-POINT FAZER A REQUISIÇÃO  DA API */
 
 // offset e limit foram criadas para controle dinâmino da páginação.
-const offset = 0
-const limit = 10
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 // recebe um pokemon e retorna uma li com os dados do pokemon no layout definido.
 function convertPokemonToLi(pokemon) {
@@ -25,22 +22,15 @@ function convertPokemonToLi(pokemon) {
 }
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:3950895211.
 const pokemonList = document.getElementById(`pokemonList`); //captura do html pelo id o elemento de lista
-
+const listaItems = []
 //ESTRUTURA DE INTERFACE DE UM FETCH
 
-// Fazendo a Requisição para a API
-fetch(url)
-    .then((response) => response.json()) // converte a promise recebida em json
-    .then((jsonBody) => jsonBody.results) // consome da estrutura Objeto results os pokemons
-    // pokemons aqui é a referencia de results acima
-    .then((pokemons)=>{
-        // percorre a lista de pokemons capturando cada um enquanto ainda tiver pokemons na lista
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            // chama função convertPokemonToLi(pokemon) para converte a estrutura em do html pronta e aproveitar o layout
-            pokemonList.innerHTML += convertPokemonToLi(pokemon);
-        }
-    })
-    .catch((error) => console.log(error));
 
+pokeApi.getPokemon().then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        listaItems.push(convertPokemonToLi(pokemon));
+      }
+      console.log(listaItems);
+})
 
---
+++ assets/js/poke-api.js
@@ -0,0 +1,11 @@
+const pokeApi = {}
+
+// FUNÇÃO PARA CONSUMIR OS DADOS DA API
+pokeApi.getPokemon = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    return fetch(url)
+    .then((response) => response.json()) // converte a promise recebida em json
+    .then((jsonBody) => jsonBody.results) // consome da estrutura Objeto results os pokemons
+    .catch((error) => console.log(error)); // verificação de erro na requisição
+}
